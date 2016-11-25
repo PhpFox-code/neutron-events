5bc5356f11a13b25f71a9f1cdea3d0a31dc91d20
Nam Nguyen <nam.ngvan@gmail.com>
2016-11-25 21:55:11 +0700

    no message

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,1 @@
 <?php
-
-namespace {
-
-    use Phpfox\EventManager\EventManager;
-
-    /**
-     * @return EventManager
-     */
-    function events()
-    {
-        return EventManager::instance();
-    }
-}
